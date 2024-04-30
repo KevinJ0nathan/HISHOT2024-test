c8c9ca55346047a480aed16931c394dd15e49514
Kevin Jonathan <142511694+KevinJ0nathan@users.noreply.github.com>
2024-04-30 20:19:07 +0700

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -2,9 +2,8 @@ var lastScrollTop = 0;
 
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
-
-    var navbar = document.getElementById("navbar")
-
+    var navbar = document.getElementById("navbar");
+    var navbarHeight = navbar.offsetHeight;
 
     if (scrollPosition > 50) {
         navbar.style.backgroundColor = 'black';
@@ -12,20 +11,18 @@ window.addEventListener('scroll', function() {
         navbar.style.backgroundColor = 'transparent';
     }
 
+    var scrollDirection = scrollPosition > lastScrollTop ? 'down' : 'up';
+    lastScrollTop = scrollPosition <= 0 ? 0 : scrollPosition; 
 
-     var scrollDirection = scrollPosition > lastScrollTop ? 'down' : 'up';
-     lastScrollTop = scrollPosition <= 0 ? 0 : scrollPosition; 
-
-     if (scrollDirection === "down") {
-        navbar.style.transition = 'opacity 0.5s';
-        navbar.style.opacity = '0';
-        navbar.style.pointerEvents = 'none';
+    if (scrollDirection === "down") {
+        navbar.style.transition = 'transform 0.5s';
+        navbar.style.transform = 'translateY(-' + navbarHeight + 'px)';
     } else {
-        navbar.style.transition = 'opacity 0.5s';
-        navbar.style.opacity = '1';
-        navbar.style.pointerEvents = 'auto';
+        navbar.style.transition = 'transform 0.5s';
+        navbar.style.transform = 'translateY(0)';
     }
 });
+
 
 document.getElementById('navbar-toggler').addEventListener('click', function() {
     document.getElementById('navbar').classList.toggle('collapsed-menu');
